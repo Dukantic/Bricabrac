0c6d515212f0bf62551418559cf282e81666484f
Dukantic <aubindo@outlook.fr>
2023-10-28 08:58:45 +0200

    calcul of kiss and handshake and print it

--- main.py
+++ main.py
@@ -18,8 +18,15 @@ def question_input(display_sentence):
     return entrer
 
 
-number_people = question_input("Number of people in your event : ")
 number_women = question_input("Number of women in your event : ")
 number_men = question_input("Number of men in your event : ")
-number_boy = question_input('Number of little boys in your event ')
+number_boy = question_input('Number of little boys in your event : ')
+number_kiss = question_input("Number of kiss you do with one person : ")
+number_people = number_women + number_men + number_boy
 
+
+
+number_total_kiss = (number_people + number_boy) * (number_people-1) * number_kiss
+number_total_handshake = number_men * (number_men - 1)
+
+print(f"In your event, for a total of {number_people} peole, the number of kisses is {number_total_kiss}, the number of handshake is {number_total_handshake}.")
