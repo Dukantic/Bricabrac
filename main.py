5f5dd50844957596233a71acadc03c5d96b6879c
Dukantic <aubindo@outlook.fr>
2023-10-28 09:06:12 +0200

    change string print and add number of transmitting

--- main.py
+++ main.py
@@ -23,10 +23,13 @@ number_men = question_input("Number of men in your event : ")
 number_boy = question_input('Number of little boys in your event : ')
 number_kiss = question_input("Number of kiss you do with one person : ")
 number_people = number_women + number_men + number_boy
-
+interation = number_people * (number_people - 1)
 
 
 number_total_kiss = (number_people + number_boy) * (number_people-1) * number_kiss
 number_total_handshake = number_men * (number_men - 1)
-
-print(f"In your event, for a total of {number_people} peole, the number of kisses is {number_total_kiss}, the number of handshake is {number_total_handshake}.")
+print('~' * 50)
+print(f"""In your event, for a total of {number_people} peole, \n
+the number of kisses is {number_total_kiss}, \n
+the number of handshake is {number_total_handshake},\n
+the number of possibilities for transmitting the disease is {interation}.""")
