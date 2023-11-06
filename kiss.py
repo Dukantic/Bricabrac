d0cdc16a28adb529def7c1a2c78c3825822325b8
Dukantic <aubindo@outlook.fr>
2023-11-06 12:21:33 +0100

    correction calcul

--- kiss.py
+++ kiss.py
@@ -1,4 +1,4 @@
-
+import math
 def kiss():
     def question_input(display_sentence):
         entrer = input(display_sentence)
@@ -22,13 +22,18 @@ def kiss():
     number_women = question_input("Number of women in your event : ")
     number_men = question_input("Number of men in your event : ")
     number_boy = question_input('Number of little boys in your event : ')
-    number_kiss = question_input("Number of kiss you do with one person : ")
+    number_kiss = abs(question_input("Number of kiss you do with one person : "))
+
+    if number_kiss == 0:
+        number_men += number_women + number_boy
+        number_women = 0
+        number_boy = 0
+
     number_people = number_women + number_men + number_boy
-    interation = number_people * (number_people - 1)
+    interation = math.comb(number_people, 2)
+    number_total_kiss =  (number_women+ number_boy) * (number_people-1) * number_kiss 
+    number_total_handshake = math.comb(number_men,2)
 
-
-    number_total_kiss = (number_women+ number_boy) * (number_people-1) * number_kiss 
-    number_total_handshake = number_men * (number_men - 1)
     print('~' * 50)
     print(f"""In your event, for a total of {number_people} peoples, \n
     the number of kisses is {number_total_kiss}, \n
