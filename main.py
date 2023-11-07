c0a256b2c5fcd8bfef4663049f0ab3aab768abb5
Dukantic <aubindo@outlook.fr>
2023-11-07 10:10:51 +0100

    user input choicefunction

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from kiss import kiss
 
-kiss()
+txt_user_enter = "User say : "
+LISTE_FONCTION = {"kiss" : kiss,
+                  "test" : "bla"}
+
+print(f"Welcome to Bricabrac, enter the function you want to execute :{list(LISTE_FONCTION.keys())} :")
+user_input = input(txt_user_enter)
+while user_input not in LISTE_FONCTION:
+    print("_" * 50  + "\n"+ "Not valid ! ")
+    print(f"Enter the function you want to execute :{list(LISTE_FONCTION.keys())} :")
+    user_input = input(txt_user_enter)
+print('_'* 50)
+LISTE_FONCTION[user_input]()
