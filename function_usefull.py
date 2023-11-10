0a0bdfcc75dde67f35a907e1639029664d4b0f69
Dukantic <aubindo@outlook.fr>
2023-11-10 12:34:42 +0100

    add bool_input

--- function_usefull.py
+++ function_usefull.py
@@ -21,4 +21,19 @@ def number_input(display_sentence):
 def description():
     for string in LISTE_EXPLANATION:
         print(string)
+
+def bool_input(display_sentence):
+    entrer = input(display_sentence)
+    valid = False
+    if entrer == 'True':
+        return True
+    elif entrer == 'False':
+        return False
     
+    while not valid:
+        print("_" * 50  + "\n"+ "Not valid ! ")
+        entrer = input(display_sentence)
+        if entrer == 'True':
+            return True
+        elif entrer == 'False':
+            return False 
